1117d9355ec34c6e25c8b10ffee6350b2f0d08c6
ANYIEKPON <anibros88@gmail.com>
2024-03-19 18:07:55 -0700

    last

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,11 +1,37 @@
+'use client'
+
 import BackImg from '@/components/backImg/backImg';
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 import { RiMailSendLine } from "react-icons/ri";
 import { ImLocation2 } from "react-icons/im";
 import { MdPhoneCallback } from "react-icons/md";
 import styles from "../contact/contact.module.scss";
 
 const page = () => {
+
+  const form = useRef();
+  const [confirmText, setConfirmText] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+    .sendForm('service_aegynk8', 'template_3k4e2rp', form.current, {
+      publicKey: '9n28nshroMNRka-HJ',
+    })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          e.target.reset();
+          setConfirmText(true);
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
   return (
     <div className={styles.contact}>
         <div className={styles.wrapper}>
@@ -22,14 +48,15 @@ const page = () => {
                     </ul>
                   </div>
                   <div className={styles.second}>
-                    <form>
-                      <input type="text" placeholder="Name *" />
-                      <input type="email" placeholder="Email *" />
-                      <input type="number" placeholder="Phone *" />
-                      <input type="text" placeholder="Company" />
-                      <textarea name="" id="" cols="30" rows="5">Message *</textarea>
-                      <input type="submit" className={styles.btn} placeholder="SEND MESSAGE"/>
+                    <form ref={form} onSubmit={sendEmail}>
+                        <input type="text" placeholder="Name *" name="name" />
+                        <input type="email" placeholder="Email *" name="email" />
+                        <input type="number" placeholder="Phone *" name="phone" />
+                        <input type="text" placeholder="Company" name="company" />
+                      <textarea placeholder="Message *" name="message" id="" cols="30" rows="5" />
+                      <input type="submit" value="Send"  className={styles.btn} />
                     </form>
+                    <span className={styles.confirm}>{confirmText ? "Message Sent" : ""}</span>
                   </div>
               </div>
             </div>
--- src/components/encrypt/Encrypt.js
+++ src/components/encrypt/Encrypt.js
@@ -1,7 +1,7 @@
 'use client';
-'use client'
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styles from '../encrypt/Encrypt.module.scss';
+import emailjs from '@emailjs/browser';
 import Image from 'next/image';
 import encrypt from '../../../public/encrypt.png';
 
@@ -9,6 +9,28 @@ const Contact = () => {
 
   const [show, setShow] = useState(false);
   const location = typeof window !== "undefined" && window.location.pathname;
+  const form = useRef();
+  const [confirmText, setConfirmText] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_aegynk8', 'template_3k4e2rp', form.current, {
+        publicKey: '9n28nshroMNRka-HJ',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          e.target.reset();
+          setConfirmText(true);
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
 
   useEffect(() => {
     
@@ -24,34 +46,26 @@ const Contact = () => {
 
   return (
     <div className={styles.encrypt}>
-        {/* {
-          show && (
-            <div className={styles.ccntact}>
-              <h3>Ready to find more</h3>
-              <p>
-                Our staff are available to provide advice and respond to your specific questions. Please email us by following the form below:
-              </p>
-            </div>
-          )
-        } */}
         <div className={styles.contactFrm}>
           <h3>CONTACT US</h3>
           <div className={styles.div}>
-            <form className={styles.form}>
+            <form className={styles.form} ref={form} onSubmit={sendEmail}>
                 <div className={styles.formInput}>
                   <label>Enter Your Name *</label>
-                  <input type="text" placeholder="Name" />
+                  <input type="text" placeholder="Name *" name="name" />
                 </div>
                 <div className={styles.formInput}>
                   <label>Enter Your Email *</label>
-                  <input type="email" placeholder="Email" />
+                  <input type="email" placeholder="Email *" name="email" />
                 </div >
                 <div className={styles.formInput}>
                   <label>Type your Message Here *</label>
-                  <textarea placeholder="Message" rows="5" cols="30"  className={styles.formInputMessage} />
+                  <textarea placeholder="Message" name="message" rows="5" cols="30"  className={styles.formInputMessage} />
                 </div>
                 <input className={styles.btn} type="submit" />
+                <span className={styles.confirm}>{confirmText ? "Message Sent" : ""}</span>
             </form>
+            
           </div>
         </div>
         <div></div>
